f64c2dc358ea394b51def21d64588dc6c4e282f6
Eugen Stan <eugen.stan@netdava.com>
2023-11-09 16:45:18 +0200

    Upgrade babashka 1.3.186

--- bin-guix/packages/babashka.scm
+++ bin-guix/packages/babashka.scm
@@ -1,24 +1,24 @@
 (define-module (bin-guix packages babashka)
-   #:use-module (guix packages)
-   #:use-module (guix download)
-   #:use-module (guix build-system copy)
-   #:use-module ((guix licenses) #:prefix licenses:))
+  #:use-module (guix packages)
+  #:use-module (guix download)
+  #:use-module (guix build-system copy)
+  #:use-module ((guix licenses) #:prefix licenses:))
 
 (define-public babashka
   (package
-    (name "babashka")
-    (version "1.3.184")
-    (source (origin
-             (method url-fetch)
-             (uri (string-append "https://github.com/babashka/babashka/releases/download/v"
-                                 version "/babashka-" version "-linux-amd64-static.tar.gz" ))
-             (sha256
-              (base32 "0jikllj23xwqx0mc114c0670baxpsx5fx6agh34a5jnf8fz8n3gr"))))
-    (build-system copy-build-system)
-    (supported-systems '("x86_64-linux" "i686-linux"))
-    (arguments
-     `(#:install-plan '(("bb" "bin/"))))
-    (synopsis "Native, fast starting Clojure interpreter for scripting")
-    (description "Babashka is a native Clojure interpreter for scripting with fast startup. Its main goal is to leverage Clojure in places where you would be using bash otherwise.")
-    (home-page "https://babashka.org/")
-    (license licenses:epl1.0)))
+   (name "babashka")
+   (version "1.3.186")
+   (source (origin
+            (method url-fetch)
+            (uri (string-append "https://github.com/babashka/babashka/releases/download/v"
+                                version "/babashka-" version "-linux-amd64-static.tar.gz" ))
+            (sha256
+             (base32 "17qvbx4c7b6951qr7r26nrsyzw3zlc0xlnwhcdl1lw9rbanxd4ps"))))
+   (build-system copy-build-system)
+   (supported-systems '("x86_64-linux" "i686-linux"))
+   (arguments
+    `(#:install-plan '(("bb" "bin/"))))
+   (synopsis "Native, fast starting Clojure interpreter for scripting")
+   (description "Babashka is a native Clojure interpreter for scripting with fast startup. Its main goal is to leverage Clojure in places where you would be using bash otherwise.")
+   (home-page "https://babashka.org/")
+   (license licenses:epl1.0)))
